3ca8df2467090565a18d660b2bce53430f6dc08f
Pitu <heyitspitu@gmail.com>
2019-05-17 23:17:09 +0900

    Add images and metadata

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,23 +20,24 @@ export default {
 			{ hid: 'keywords', name: 'keywords', content: `${process.env.META_KEYWORDS}` },
 			{ hid: 'apple-mobile-web-app-title', name: 'apple-mobile-web-app-title', content: `${process.env.SERVICE_NAME}` },
 			{ hid: 'application-name', name: 'application-name', content: `${process.env.SERVICE_NAME}` },
-			{ hid: 'msapplication-config', name: 'msapplication-config', content: `${process.env.DOMAIN}/browserconfig.xml` },
+			{ hid: 'msapplication-TileColor', name: 'msapplication-TileColor', content: `#2b5797` },
+			{ hid: 'msapplication-config', name: 'msapplication-config', content: `${process.env.domain}/assets/browserconfig.xml` },
 			{ hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
 			{ hid: 'twitter:site', name: 'twitter:site', content: `${process.env.META_TWITTER_HANDLE}` },
 			{ hid: 'twitter:creator', name: 'twitter:creator', content: `${process.env.META_TWITTER_HANDLE}` },
 			{ hid: 'twitter:title', name: 'twitter:title', content: `${process.env.SERVICE_NAME}` },
 			{ hid: 'twitter:description', name: 'twitter:description', content: `${process.env.META_DESCRIPTION}` },
-			{ hid: 'twitter:image', name: 'twitter:image', content: `${process.env.DOMAIN}/share.jpg` },
+			{ hid: 'twitter:image', name: 'twitter:image', content: `${process.env.domain}/assets/share.jpg` },
 			{ hid: 'og:url', property: 'og:url', content: `${process.env.DOMAIN}` },
 			{ hid: 'og:type', property: 'og:type', content: 'website' },
 			{ hid: 'og:title', property: 'og:title', content: `${process.env.SERVICE_NAME}` },
 			{ hid: 'og:description', property: 'og:description', content: `${process.env.META_DESCRIPTION}` },
-			{ hid: 'og:image', property: 'og:image', content: `${process.env.DOMAIN}/share.jpg` },
-			{ hid: 'og:image:secure_url', property: 'og:image:secure_url', content: `${process.env.DOMAIN}/share.jpg` },
+			{ hid: 'og:image', property: 'og:image', content: `${process.env.domain}/assets/share.jpg` },
+			{ hid: 'og:image:secure_url', property: 'og:image:secure_url', content: `${process.env.domain}/assets/share.jpg` },
 			{ hid: 'og:site_name', property: 'og:site_name', content: `${process.env.SERVICE_NAME}` }
 		],
 		link: [
-			{ type: 'application/json+oembed', href: `${process.env.DOMAIN}/oembed.json` }
+			{ type: 'application/json+oembed', href: `${process.env.domain}/assets/oembed.json` }
 		]
 	},
 	css: [],
